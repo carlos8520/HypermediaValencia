fe33ca025b7e182a37a61752ea932781c372e5dc
20136357 CARLOS ABRAHAM VALENCIA TORRES <20136357@TELEMATICA.UCOL.MX>
2019-08-30 07:38:01 -0500

    changed changed x2

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
  
 app.get('/', function (req, res) {
-  res.send('Esta wea no jala');
+  res.send('goodbye horses, am flying over you');
 });
  
 app.listen(PORT);
