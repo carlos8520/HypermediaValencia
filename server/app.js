318f20230c26f85c9a5e7916fc6565a3146f9c5e
20136357 CARLOS ABRAHAM VALENCIA TORRES <20136357@TELEMATICA.UCOL.MX>
2019-09-06 08:31:06 -0500

    refactory

--- server/app.js
+++ server/app.js
@@ -1,26 +1,9 @@
 const express = require('express');
-const nodemon = require('nodemon');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+const passportConfig = require('./services/passsport');
 
 const app = express();
+require('./routes/authRoutes')(app);
+
 const PORT = process.env.PORT || 3000;
-
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-    }, (accessToken) => {
-        console.log(accessToken);
-    })
-);
-
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}));
-
-app.get('/auth/google/callback', passport.authenticate('google'));
-
 app.listen(PORT);
 
