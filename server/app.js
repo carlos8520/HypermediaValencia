5a77e4a6d61c7c94f196a985d666429055dff7a1
20136357 CARLOS ABRAHAM VALENCIA TORRES <20136357@TELEMATICA.UCOL.MX>
2019-09-13 08:27:38 -0500

    mongo

--- server/app.js
+++ server/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
+require('./models/User');
 const passportConfig = require('./services/passsport');
+const mongoose = require('mongoose');
+
+
+const keys = require('./config/keys')
 
 const app = express();
+
+
+mongoose.connect(keys.mongooseURI);
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 3000;
